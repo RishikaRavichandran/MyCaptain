fc92a015ba1a9724d8cfd0eef5fe80eaadae6b3d
RISHIKA RAVICHANDRAN <86842753+RishikaRavichandran@users.noreply.github.com>
2021-07-09 10:31:35 +0530

    Add files via upload

---
+++ Challange- Conditional Statements - Grades and Marks.c
@@ -0,0 +1,28 @@
+#include<sstdio.h
+main()
+{ 
+int x;
+printf(" Enter marks : ");
+scanf("%d", &x);
+if(x>=85 && x<100)
+{
+	printf(" GRADE A ");
+}
+else if(x>=70 && x<84)
+{
+	printf(" GRADE B ");
+}
+else if(x>=55 && x<69)
+{
+	printf(" GRADE C ");
+}
+else if(x>=40 && x<54)
+{
+	printf(" GRADE D ");
+}
+else
+{
+	printf(" GRADE F ");
+}
+return 0
+}
