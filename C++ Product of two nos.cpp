5e121ab6da51f85fcc15676eea1641281f453e07
RISHIKA RAVICHANDRAN <86842753+RishikaRavichandran@users.noreply.github.com>
2021-08-03 15:54:25 +0530

    Add files via upload

---
+++ C++ Product of two nos.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+void main()
+{
+	int a, b, product;
+	cout<<"Enter the first no. :";
+	cin>>a;
+	cout<<"Enter the second no. :";
+	cin>>b;
+	cout<<"Product = "<<product;
+	return 0;
+}
+
