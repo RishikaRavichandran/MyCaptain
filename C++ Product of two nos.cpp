de8b357a0ea8e2e2c1b9e0b5f7614a09057d1a66
RISHIKA RAVICHANDRAN <86842753+RishikaRavichandran@users.noreply.github.com>
2021-08-03 16:01:08 +0530

    Update C++ Product of two nos.cpp

--- C++ Product of two nos.cpp
+++ C++ Product of two nos.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
-void main()
+using namespace std;
+main()
 {
 	int a, b, product;
 	cout<<"Enter the first no. :";
 	cin>>a;
 	cout<<"Enter the second no. :";
 	cin>>b;
+	product = a*b;
 	cout<<"Product = "<<product;
 	return 0;
 }
